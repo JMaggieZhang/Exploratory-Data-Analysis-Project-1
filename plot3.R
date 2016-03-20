394218891a5022c318f4e48c00ed18ea5c9045b9
JMaggieZhang <jmaggiezhang@gmail.com>
2016-03-20 16:18:59 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -10,3 +10,16 @@ subdata <- subset(data, data$Date %in% c("1/2/2007","2/2/2007"))
 # convert datetime
 datetime <- strptime(paste(subdata$Date, subdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 
+# making Plot3
+with(data, {
+  plot(Sub_metering_1~datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~datetime,col='Red')
+  lines(Sub_metering_3~datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
